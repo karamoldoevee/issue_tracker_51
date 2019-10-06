3b447b3c87edfdf51fa8e5dca9f3a9b7e98c045c
karamoldoevee <karamoldoevee@gmail.com>
2019-10-06 06:53:28 +0600

    Написан самописный класс UpdateView для Задач

---
+++ source/webapp/views/base_view.py
@@ -0,0 +1,59 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views import View
+from django.views.generic import TemplateView
+
+
+class ListView(TemplateView):
+    context_key = 'objects'
+    model = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context[self.context_key] = self.get_objects()
+        return context
+
+    def get_objects(self):
+        return self.model.objects.all()
+
+
+class DetailView(TemplateView):
+    context_key = 'object'
+    model = None
+    key_kwarg = 'pk'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context[self.context_key] = self.get_object()
+        return context
+
+    def get_object(self):
+        pk = self.kwargs.get(self.key_kwarg)
+        return get_object_or_404(self.model, pk=pk)
+
+
+class CreateView(View):
+    form_class = None
+    template_name = None
+    model = None
+    redirect_url = None
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def get_redirect_url(self):
+        return self.redirect_url
+
+    def form_valid(self, form):
+        self.object = self.model.objects.create(**form.cleaned_data)
+        return redirect(self.get_redirect_url())
+
+    def form_invalid(self, form):
+        return render(self.request, self.template_name, context={'form': form})
--- source/webapp/views/views.py
+++ source/webapp/views/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView
+
 from webapp.forms import IssueForm
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
 from webapp.models import Issue
+from django.core.paginator import Paginator
 
 
 
@@ -13,12 +15,14 @@ class IndexView(ListView):
 
     context_object_name = 'issues'
 
+    model = Issue
+
+    ordering = ['-created_at']
+
     paginate_by = 5
 
     paginate_orphans = 1
 
-    def get_queryset(self):
-        return Issue.objects.all().order_by('-created_at')
 
 class IssueView(DetailView):
 
@@ -38,9 +42,31 @@ class IssueCreateView(CreateView):
         return reverse('index')
 
 
-def issue_update_view(request, pk):
-    issue = get_object_or_404(Issue, pk=pk)
-    if request.method == 'GET':
+# def issue_update_view(request, pk):
+#     issue = get_object_or_404(Issue, pk=pk)
+#     if request.method == 'GET':
+#         form = IssueForm(data={
+#             'summary': issue.summary,
+#             'description': issue.description,
+#             'status': issue.status_id,
+#             'type': issue.type_id
+#         })
+#         return render(request, 'issue/update.html', context={'form': form, 'issue': issue})
+#     elif request.method == 'POST':
+#         form = IssueForm(data=request.POST)
+#         if form.is_valid():
+#             issue.summary = form.cleaned_data['summary']
+#             issue.description = form.cleaned_data['description']
+#             issue.status = form.cleaned_data['status']
+#             issue.type = form.cleaned_data['type']
+#             issue.save()
+#             return redirect('issue_view', pk=issue.pk)
+#         else:
+#             return render(request, 'issue/update.html', context={'form': form, 'issue': issue})
+
+class issue_update_view(View):
+    def get(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=kwargs.get('pk'))
         form = IssueForm(data={
             'summary': issue.summary,
             'description': issue.description,
@@ -48,7 +74,9 @@ def issue_update_view(request, pk):
             'type': issue.type_id
         })
         return render(request, 'issue/update.html', context={'form': form, 'issue': issue})
-    elif request.method == 'POST':
+
+    def post(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=kwargs.get('pk'))
         form = IssueForm(data=request.POST)
         if form.is_valid():
             issue.summary = form.cleaned_data['summary']

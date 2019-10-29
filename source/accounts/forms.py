c14fbb4f257ce11f8486f35748fc90eb4d3d3b80
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-10-29 19:05:10 +0600

    Добавлена валидация пустого first_name

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.models import User
 from django import forms
-
+from django.core.exceptions import ValidationError
 
 class UserCreationForm(forms.ModelForm):
     password = forms.CharField(label="Пароль", strip=False, widget=forms.PasswordInput)
 
     password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+
+    email = forms.EmailField(required=True, label='Email')
+
+    # first_name = forms.CharField(max_length=200, required=False, label='first_name')
 
     def clean_password_confirm(self):
 
@@ -17,6 +21,14 @@ class UserCreationForm(forms.ModelForm):
             raise forms.ValidationError('Пароли не совпадают!')
 
         return password_confirm
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        try:
+            User.objects.get(first_name=None)
+            raise ValidationError('This filed cant be empty.', code='firt_name_empty_filed')
+        except User.DoesNotExist:
+            return first_name
 
     def save(self, commit=True):
 

d21c29d85d6bfa5490156e1bbc953cb75fcd6195
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-11-14 17:08:43 +0600

    Добавлен PermissionRequiredMixin для проектов

--- source/webapp/views/project_views.py
+++ source/webapp/views/project_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 
@@ -70,12 +71,16 @@ class ProjectView(DetailView):
     model = Project
 
 
-class ProjectCreateView(CreateView):
+class ProjectCreateView(PermissionRequiredMixin, CreateView):
     template_name = 'project/create.html'
 
     model = Project
 
     fields = ['name', 'description']
+
+    permission_required = 'webapp.add_project'
+
+    permission_denied_message = "Доступ запрещён"
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -87,17 +92,21 @@ class ProjectCreateView(CreateView):
         return reverse('webapp:project_index')
 
 
-class ProjectUpdateView(UpdateView):
+class ProjectUpdateView(PermissionRequiredMixin, UpdateView):
     model = Project
 
     template_name = 'project/update.html'
 
     fields = ['name', 'description']
 
+    permission_required = 'webapp.change_project'
+
+    permission_denied_message = "Доступ запрещён"
+
     def get_success_url(self):
         return reverse('webapp:project_view', kwargs={'pk': self.object.pk})
 
-class ProjectDeleteView(DeleteView):
+class ProjectDeleteView(PermissionRequiredMixin, DeleteView):
 
     template_name = 'project/delete.html'
 
@@ -106,6 +115,10 @@ class ProjectDeleteView(DeleteView):
     context_object_name = 'project'
 
     success_url = reverse_lazy('webapp:project_index')
+
+    permission_required = 'webapp.delete_project'
+
+    permission_denied_message = "Доступ запрещён"
 
 
 

9074974e6d891dad14a8efd4b943e622dbbe6876
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-11-17 19:29:17 +0600

    Добавлен mixins.py с классои статистики

---
+++ source/webapp/mixins.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+from django.views.generic import View
+
+class StatisticsMixin(View):
+    def get(self, request, *args, **kwargs):
+        stats = request.session.get('stats', {})
+
+        visits = stats.get('visits', {})
+
+        current_page_visits = visits.get(request.path, 0)
+        current_page_visits += 1
+        visits[request.path] = current_page_visits
+
+        total_visits = visits.get('total', 0)
+        total_visits += 1
+        visits['total'] = total_visits
+
+        stats['visits'] = visits
+
+        times = stats.get('times', {})
+        now = datetime.now()
+        last_time = request.session.get('last_time')
+        last_page = request.session.get('last_page')
+        if last_page:
+            last_time = datetime.strptime(last_time, '%Y-%m-%d %H:%M:%S')
+            time_diff = (now - last_time).total_seconds()
+
+            last_page_time = times.get(last_page, 0)
+            last_page_time += time_diff
+            times[last_page] = last_page_time
+
+            total_time = times.get('total', 0)
+            total_time += time_diff
+            times['total'] = total_time
+
+        stats['times'] = times
+
+        request.session['stats'] = stats
+        request.session['last_time'] = now.strftime('%Y-%m-%d %H:%M:%S')
+        request.session['last_page'] = request.path
+
+        return super().get(request, *args, **kwargs)
